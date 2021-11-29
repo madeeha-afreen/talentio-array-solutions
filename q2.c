6254f0da521743b9966cefbe0cf0272ecf8663b9
Madeeha Afreen <91774047+madeeha-afreen@users.noreply.github.com>
2021-11-29 20:22:23 +0530

    created q2.c

---
+++ q2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() 
+{
+    int n, flag=0;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+        if (arr[i]==1)
+        {
+            flag=1;
+        }
+    }
+    if(flag)
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("1");
+    }
+    return 0;
+}
